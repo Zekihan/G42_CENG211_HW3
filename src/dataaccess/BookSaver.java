05eb009ffdf1ca5fce30bdb90166022be729855b
Zekihan <38537758+Zekihan@users.noreply.github.com>
2018-11-16 19:47:15 +0300

    saver json

--- src/dataaccess/BookSaver.java
+++ src/dataaccess/BookSaver.java
@@ -1,5 +1,34 @@
 package dataaccess;
 
+import org.json.simple.JSONObject;
+
+import business.Book;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class BookSaver {
 
+	@SuppressWarnings("unchecked")
+	public void saver(Book book) {
+		
+		JSONObject books = new JSONObject();
+		books.put("itemno", book.getItemNo());
+		books.put("name", book.getName());
+		books.put("author", book.getAuthor());
+		books.put("publisher", book.getPublisher());
+		
+		try {
+
+			FileWriter file = new FileWriter(book.getItemNo()+".json");
+			file.write(books.toJSONString());
+			file.flush();
+			file.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.print(books.toJSONString());
+	}
 }
--- src/dataaccess/MovieSaver.java
+++ src/dataaccess/MovieSaver.java
@@ -1,5 +1,44 @@
 package dataaccess;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import business.Movie;
+
 public class MovieSaver {
 
+	@SuppressWarnings("unchecked")
+	public void saver(Movie movie) {
+		
+		JSONObject movies = new JSONObject();
+		
+		movies.put("itemno", movie.getItemNo());
+		movies.put("name", movie.getName());
+		movies.put("genre", movie.getGenre());
+		movies.put("producer", movie.getProducer());
+		
+		JSONArray actors = new JSONArray();
+		
+		for (String actor : movie.getActors()) {
+			actors.add(actor);
+		}
+		
+		movies.put("actors", actors);
+		
+		try {
+
+			FileWriter file = new FileWriter(movie.getItemNo()+".json");
+			file.write(movies.toJSONString());
+			file.flush();
+			file.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.print(movies.toJSONString());
+	}
 }
