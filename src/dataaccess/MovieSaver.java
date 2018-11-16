dc0e17015a100ebd5351f46e17ff69115493005e
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2018-11-16 21:31:45 +0300

    Update MovieSaver.java

--- src/dataaccess/MovieSaver.java
+++ src/dataaccess/MovieSaver.java
@@ -1,10 +1,23 @@
 package dataaccess;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import business.Movie;
 
@@ -37,6 +50,58 @@ public class MovieSaver {
 
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public void saverXml(Movie movie) {
+		try {
+			
+			DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+
+			DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+
+			Document document = documentBuilder.newDocument();
+
+			Element root = document.createElement("item");
+			document.appendChild(root);
+
+			Element movieItem = document.createElement("movie");
+
+			root.appendChild(movieItem);
+
+			Attr attr = document.createAttribute("id");
+			attr.setValue(movie.getItemNo() + "");
+			movieItem.setAttributeNode(attr);
+
+			Element name = document.createElement("name");
+			name.appendChild(document.createTextNode(movie.getName()));
+			movieItem.appendChild(name);
+
+			Element genre = document.createElement("genre");
+			genre.appendChild(document.createTextNode(movie.getGenre()));
+			movieItem.appendChild(genre);
+
+			Element producer = document.createElement("producer");
+			producer.appendChild(document.createTextNode(movie.getProducer()));
+			movieItem.appendChild(producer);
+			
+			Element actors = document.createElement("actors");
+			actors.appendChild(document.createTextNode(movie.getActors().toString()));
+			movieItem.appendChild(actors);
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource domSource = new DOMSource(document);
+			StreamResult streamResult = new StreamResult(new File(movie.getItemNo() + ".xml"));
+
+			transformer.transform(domSource, streamResult);
+
+			System.out.println("Done creating XML File");
+
+		} catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} catch (TransformerException tfe) {
+			tfe.printStackTrace();
 		}
 	}
 }
